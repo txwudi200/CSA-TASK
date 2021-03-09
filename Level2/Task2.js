7551caf8caa545f94ac23deb707823a36010018b
Silvercrown <56759170+txwudi200@users.noreply.github.com>
2021-03-09 12:28:05 +0800

    Delete Level2 directory

--- Level2/Task2.js
+++
@@ -1,7 +0,0 @@
-var ary = [1, [2, [3, [4, 5]]], 6];
-
-function even(ary) {
-    return ary.toString().split(',').map(function(ary) { return Number(ary) })
-}
-
-console.log(even(ary)); //[1,2,3,4,5,6]
