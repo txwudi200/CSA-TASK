0088cf2015172e8b36e5dce0b7acfcb887a8cf30
Silvercrown <56759170+txwudi200@users.noreply.github.com>
2021-03-09 12:24:43 +0800

    Task-2nd

---
+++ secondtask2/task2-2.js
@@ -0,0 +1,11 @@
+var arr = [
+    [1, 2],
+    [3, 4, ],
+    [6, 7, 9, [11, 12, [12, 13, [14]]]], 10
+];
+
+function even(arr) {
+    return arr.toString()
+}
+
+console.log(even(arr)); // [ 1, 2, 3, 4, 6, 7, 9, 11, 12, 12, 13, 14, 10 ]
